8607c642da4b4eadc1a95f3ebeef61e08ec96a6a
jradziejewski <192462@student.ue.wroc.pl>
2025-01-16 17:49:04 +0100

    add getnextfeedtofetch

--- sql/queries/feed.sql
+++ sql/queries/feed.sql
@@ -27,3 +27,8 @@ DELETE FROM feeds;
 UPDATE feeds
 SET last_fetched_at = $1, updated_at = $1
 WHERE feeds.id = $2;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at NULLS FIRST
+LIMIT 1;
